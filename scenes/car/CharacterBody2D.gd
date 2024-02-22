34c8aba045c80ff71dcf67052e760a09e764353d
stndaru <stefanus.ndaru@ui.ac.id>
2024-02-23 02:07:14 +0700

    Add rpm delay and more comments

--- scenes/car/CharacterBody2D.gd
+++ scenes/car/CharacterBody2D.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 # General Default Value
-var turn = 0 # rate at which steer angle increases
-var turn_rate = 0.5 # rate of change for turn
-var gas = 0 # rate at which rpm increases
-var gas_rate = 0.5 # rate of change for gas
+var turn = 0 # Rate at which steer angle increases
+var turn_rate = 0.5 # Rate of change for turn
+var gas = 0 # Rate at which rpm increases
+var gas_rate = 0.5 # Rate of change for gas
 
 # Steering Property
 var steer_angle = 0
@@ -13,7 +13,7 @@ var steer_decay = 0.7
 # Car Manueverability Data
 var wheel_base = 70  # Distance from front to rear wheel
 var steering_angle = 15  # Amount that front wheel turns, in degrees
-var weight = 1.2
+var weight = 1.2 # Car Weight
 
 # Car Engine Data
 var engine_power = 300  # Forward acceleration force.
@@ -23,6 +23,7 @@ var acceleration = Vector2.ZERO
 var rpm = 0
 var max_rpm = 7
 var rpm_decay = 0.5
+var rpm_delay = 0.8 # Delay of rpm following gas input
 var torque = 0
 
 # Car Friction and Drag
@@ -54,6 +55,8 @@ func _physics_process(delta):
 	velocity += acceleration / weight * delta
 	move_and_slide()
 
+# TODO Change input from using turn_rate to 0 and 1 and use axis for joystick support
+# Reference https://docs.godotengine.org/en/stable/tutorials/inputs/controllers_gamepads_joysticks.html
 func get_input():
 	# Car Steering Wheel Data, Higher Turn -> Steering Wheel Turned More
 	if Input.is_action_pressed("ui_left") || Input.is_action_pressed("ui_right"):
@@ -72,14 +75,13 @@ func get_input():
 		
 	if Input.is_action_pressed("ui_up"):
 		gas += gas_rate
-		rpm = clamp(rpm + gas, 0, max_rpm)
-		torque = rpm * engine_power
+		rpm = clamp(lerpf(rpm, rpm+gas, rpm_delay), 0, max_rpm)
 	else:
 		gas = move_toward(gas, 0, gas_rate)
 		rpm = move_toward(rpm, 0, rpm_decay)
-		torque = move_toward(torque, 0, engine_power)
 	
 	# transform.x is direction of force
+	torque = rpm * engine_power
 	acceleration = transform.x * torque
 	
 	if Input.is_action_pressed("ui_down"):
