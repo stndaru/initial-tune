32fe5bdf575071d2035ec4e16c9e115f0237744f
stndaru <stefanus.ndaru@ui.ac.id>
2024-03-12 19:28:00 +0700

    Fix bug on brake slider

--- scenes/menu/tuning/BrakeSlider.gd
+++ scenes/menu/tuning/BrakeSlider.gd
@@ -5,12 +5,12 @@ var car_data = 0
 func _ready():
 	car_data = get_tree().get_root().find_child("CarBody", true, false)
 	$Slider.set_focus_mode(0)
-	$Slider.min_value = 1
-	$Slider.max_value = 3
+	$Slider.min_value = -3
+	$Slider.max_value = -1
 	$Slider.step = 0.1
-	$Slider.value = -car_data.brake_power
+	$Slider.value = car_data.brake_power
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	$Value.text = str($Slider.value)
-	car_data.brake_power = -$Slider.value
+	car_data.brake_power = $Slider.value
